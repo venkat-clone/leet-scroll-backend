282e05ed0978c602a146ebd838480f9ffd0bc749
venkey <lingampally.venkey@gmail.com>
2025-11-28 23:03:46 +0530

    feat: configure husky and jest coverage chore: update readme fix: prevent crash on null user

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,14 @@ const customJestConfig = {
         // Handle module aliases (this will be automatically configured for you soon)
         '^@/(.*)$': '<rootDir>/$1',
     },
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 90,
+            lines: 90,
+            statements: 90,
+        },
+    },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
